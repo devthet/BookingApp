fed3e39b637e59af377aefb66c9499e9b97454aa
devthet <devthet@gmail.com>
2020-03-03 22:19:09 +0630

    Initial Commit

---
+++ BookingApp.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using BookingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingApp.API.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> x): base(x){}
+        public DbSet<Value> Values{get;set;}
+    }
+}
